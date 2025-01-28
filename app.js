4d31de7aa13c49c777face29240df83b5c7bb862
Ashish21212 <acp.aashish44@gmail.com>
2025-01-29 00:54:12 +0545

    simple server run and database connected

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const express = require('express');
-
-
+const mongoose  = require('mongoose');
+// const dotenv = require('dotenv');
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect('mongodb+srv://acpaashish44:A3J9qNIUwSawitkG@tuition.hwtxz.mongodb.net/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  
+
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
