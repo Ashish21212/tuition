930f763fceac7a920610d3952c55d0d9a97e4643
ujelooo8 <ujlpdl123@gmail.com>
2025-02-03 18:02:28 +0545

    Database connected

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
-const express = require('express');
-const mongoose  = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const classRouter = require('./routes/class');
-const userRoute = require('./routes/user');
+const classRouter = require("./routes/class");
+const userRoute = require("./routes/user");
 
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  if(req.method === "OPTIONS"){
-    res.header("Access-Control-Allow-Methods","PUT,POST,PATCH, DELETE, GET");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH, DELETE, GET");
     return res.status(200).json({});
   }
-  next()
-})
+  next();
+});
 
-app.use('/api',classRouter);
-app.use('/users',userRoute);
+app.use("/api", classRouter);
+app.use("/users", userRoute);
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  
-
-})
-.then(()=>{
-console.log('Database connected')
-})
-
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database and Server connected");
+  });
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
